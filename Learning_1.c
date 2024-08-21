347f84eaae22ad0732ff9f5efaea7578a723dd2a
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-08-21 22:37:42 +0300

    Add files via upload

---
+++ Learning_1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello C Language : ) ");
+    getchar();  // Use getchar() as a standard alternative to getch()
+    return 0;   // Return 0 to indicate successful execution
+}
