9064a37e2041406ca838c94fc63495c858d4ee9b
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-12-08 02:13:07 +0300

    update

--- Practice Problems/Problem 1.c
+++ Practice Problems/Problem 1.c
@@ -1,1 +1,16 @@
-//Problem 1
+//Problem 1 - Read your year of birth and print your age.
+
+#include <stdio.h>
+
+int main(){
+
+    int birth_year;
+    const int current_year = 2024;
+
+    printf("Enter your year of birth: ");
+    scanf("%d", &birth_year);
+
+    int age = current_year - birth_year;
+
+    printf("You are %d years old", age)
+}
