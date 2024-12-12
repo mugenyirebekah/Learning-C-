168eb8b9145c438aec5a63a8181fb4755618d90c
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-12-12 19:06:19 +0300

    video 1

---
+++ Brocode/Video_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("I like pizza \n");
+    printf("It's really good!\n");
+
+    return 0; //return 0 if there is no error...return 1 if there is an error 
+}
