541f827c3612c524f03432370a0c48a970922124
Rebekah Mugenyi <rebekahmish16@gmail.com>
2024-12-12 19:14:18 +0300

    update

---
+++ Brocode/part_2.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main(){
+
+    printf("I \nlike\npizza!\n");
+
+    printf("1\t2\t3\t\n4\t5\t6\n7\t8\t9");
+
+    printf("\n");
+
+    printf(" \"I like pizza\" was possibly said by Abraham Lincoln??");
+
+    
+
+    return 0;
+}
+
+/*Escape sequences
+
+\n - newline
+\t - tab
+
+
+
+*/
